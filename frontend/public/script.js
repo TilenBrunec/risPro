92c8dd24545f509627e103255c41fca273a34a94
TilenBrunec <tilen.brunec@student.um.si>
2024-11-12 15:38:18 +0100

    pdf se narei

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -24,11 +24,13 @@ function pokaziRecepte() {
             recepti = data.map(recept => {
                 const listItem = document.createElement('li');
                 listItem.innerHTML = `
+                    <button type="button" onclick="narediPDF();">Odpri z PDF</button>
                     <button type="button" onclick="izbrisiRecept(${recept.id});">Izbriši</button>
                     <button type="button" onclick="posodobiRecept(${recept.id}, '${recept.naziv}', '${recept.sestavine}', '${recept.potekdela}');">Spremeni</button><br>
                     <h2>${recept.naziv}</h2>
                     Sestavine: ${recept.sestavine}<br><hr>
                     Potek Dela: ${recept.potekdela}
+                   
                     
                     <!-- Hidden form -->
                     <form id="updateForm-${recept.id}" class="hide" onsubmit="submitUpdate(${recept.id}, event)">
@@ -49,6 +51,128 @@ function pokaziRecepte() {
         .catch(error => console.error('Error fetching data:', error));
 }
 
+
+function narediPDF() {
+    var pdfObject = jsPDFInvoiceTemplate.default(props);
+    console.log("Object created:",pdfObject);
+}
+var props = {
+    outputType: jsPDFInvoiceTemplate.OutputType.Save,
+    returnJsPDFDocObject: true,
+    fileName: "Invoice 2021",
+    orientationLandscape: false,
+    compress: true,
+    logo: {
+        src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
+        type: 'PNG', //optional, when src= data:uri (nodejs case)
+        width: 53.33, //aspect ratio = width/height
+        height: 26.66,
+        margin: {
+            top: 0, //negative or positive num, from the current position
+            left: 0 //negative or positive num, from the current position
+        }
+    },
+    stamp: {
+        inAllPages: true, //by default = false, just in the last page
+        src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
+        type: 'JPG', //optional, when src= data:uri (nodejs case)
+        width: 20, //aspect ratio = width/height
+        height: 20,
+        margin: {
+            top: 0, //negative or positive num, from the current position
+            left: 0 //negative or positive num, from the current position
+        }
+    },
+    business: {
+        name: "Business Name",
+        address: "Albania, Tirane ish-Dogana, Durres 2001",
+        phone: "(+355) 069 11 11 111",
+        email: "email@example.com",
+        email_1: "info@example.al",
+        website: "www.example.al",
+    },
+    contact: {
+        label: "Invoice issued for:",
+        name: "Client Name",
+        address: "Albania, Tirane, Astir",
+        phone: "(+355) 069 22 22 222",
+        email: "client@website.al",
+        otherInfo: "www.website.al",
+    },
+    invoice: {
+        label: "Invoice #: ",
+        num: 19,
+        invDate: "Payment Date: 01/01/2021 18:12",
+        invGenDate: "Invoice Date: 02/02/2021 10:17",
+        headerBorder: false,
+        tableBodyBorder: false,
+        header: [
+          {
+            title: "#", 
+            style: { 
+              width: 10 
+            } 
+          }, 
+          { 
+            title: "Title",
+            style: {
+              width: 30
+            } 
+          }, 
+          { 
+            title: "Description",
+            style: {
+              width: 80
+            } 
+          }, 
+          { title: "Price"},
+          { title: "Quantity"},
+          { title: "Unit"},
+          { title: "Total"}
+        ],
+        table: Array.from(Array(10), (item, index)=>([
+            index + 1,
+            "There are many variations ",
+            "Lorem Ipsum is simply dummy text dummy text ",
+            200.5,
+            4.5,
+            "m2",
+            400.5
+        ])),
+        additionalRows: [{
+            col1: 'Total:',
+            col2: '145,250.50',
+            col3: 'ALL',
+            style: {
+                fontSize: 14 //optional, default 12
+            }
+        },
+        {
+            col1: 'VAT:',
+            col2: '20',
+            col3: '%',
+            style: {
+                fontSize: 10 //optional, default 12
+            }
+        },
+        {
+            col1: 'SubTotal:',
+            col2: '116,199.90',
+            col3: 'ALL',
+            style: {
+                fontSize: 10 //optional, default 12
+            }
+        }],
+        invDescLabel: "Invoice Note",
+        invDesc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+    },
+    footer: {
+        text: "The invoice is created on a computer and is valid without the signature and stamp.",
+    },
+    pageEnable: true,
+    pageLabel: "Page ",
+};
+
 function izbrisiRecept(id) {
     if (confirm("Ali ste prepričani, da želite izbrisati ta recept?")) {
         fetch(`http://localhost:8080/recept/${id}`, { 
@@ -68,6 +192,31 @@ function izbrisiRecept(id) {
 function posodobiRecept(id) {
     const form = document.getElementById(`updateForm-${id}`);
     form.classList.toggle('hide'); // skrijes
+}
+
+function submitUpdate(id, event) {
+    event.preventDefault(); // prekini da se ti se nea enkrat nalozi
+
+    const naziv = document.getElementById(`naziv-${id}`).value;
+    const sestavine = document.getElementById(`sestavine-${id}`).value;
+    const potekdela = document.getElementById(`potekdela-${id}`).value; 
+
+    // PUT
+    fetch(`http://localhost:8080/recept/${id}`, { method: 'PUT',headers: {'Content-Type': 'application/json'},
+
+
+
+        body: JSON.stringify({ id, naziv, sestavine, potekdela }) 
+    })
+    .then(response => {
+        if (response.ok) {
+            alert(`Recept ${naziv} je bil uspešno posodobljen.`);
+            pokaziRecepte(); // obnovi
+        } else {
+            alert('Prišlo je do napake pri posodabljanju recepta.');
+        }
+    })
+    .catch(error => console.error('Napaka pri posodabljanju recepta:', error));
 }
 
 function dodajRecept(event) {
@@ -108,29 +257,4 @@ function dodajRecept(event) {
         console.error('Error:', error);
         alert('Prišlo je do napake pri dodajanju recepta. Prosimo, preverite konzolo za več informacij.');
     });
-}
-
-function submitUpdate(id, event) {
-    event.preventDefault(); // prekini da se ti se nea enkrat nalozi
-
-    const naziv = document.getElementById(`naziv-${id}`).value;
-    const sestavine = document.getElementById(`sestavine-${id}`).value;
-    const potekdela = document.getElementById(`potekdela-${id}`).value; 
-
-    // PUT
-    fetch(`http://localhost:8080/recept/${id}`, { method: 'PUT',headers: {'Content-Type': 'application/json'},
-
-
-
-        body: JSON.stringify({ id, naziv, sestavine, potekdela }) 
-    })
-    .then(response => {
-        if (response.ok) {
-            alert(`Recept ${naziv} je bil uspešno posodobljen.`);
-            pokaziRecepte(); // obnovi
-        } else {
-            alert('Prišlo je do napake pri posodabljanju recepta.');
-        }
-    })
-    .catch(error => console.error('Napaka pri posodabljanju recepta:', error));
 }
